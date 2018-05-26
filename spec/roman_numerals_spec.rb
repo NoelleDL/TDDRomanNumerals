234b3b2d8e1232623469ce682713fd053a8fe68d
NoelleDL <noelle.legrain@gmail.com>
2018-05-26 19:32:04 +0100

    test failing

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -4,4 +4,8 @@ describe '#convert' do
   it "returns 'I' when given 1" do
     expect(convert(1)).to eq 'I'
   end
+
+  it "returns 'X' when given 10" do
+    expect(convert(10)).to eq 'X'
+  end
 end
