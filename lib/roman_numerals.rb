30c114ac5e1a4815c5a31934952cdac0f68f3d48
NoelleDL <noelle.legrain@gmail.com>
2018-05-27 10:05:16 +0100

    Refactored loop

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -2,11 +2,8 @@ def convert(num)
   roman_numerals = { 1 => "I", 10 => "X", 100 => "C" }
   numeral = []
   roman_numerals.each do | key, value |
-    if num == key
-      return roman_numerals[num]
-    elsif num % key == 0 && num < 10
-      numeral << value * num
-    end
+    return roman_numerals[num] if num == key
+    numeral << value * num if num % key == 0 && num < 10  
   end
   numeral.join
 end
