938986fd3eeefe0d01149245ac5be31ebfe3ed98
NoelleDL <noelle.legrain@gmail.com>
2018-05-27 09:36:39 +0100

    4th test passing

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,4 +1,10 @@
 def convert(num)
   roman_numerals = { 1 => "I", 10 => "X", 100 => "C" }
-  roman_numerals[num]
+  numeral = []
+  roman_numerals.each do | key, value |
+    if num % key == 0
+      numeral << value * num
+    end
+  end
+  numeral.join
 end
